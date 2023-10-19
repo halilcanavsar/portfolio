75b0e1a9c87e22624f3192221f2d5e96ee3270d9
halilcanavsar <halilcanavsar@gmail.com>
2023-10-19 18:24:52 -0400

    hero page

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,10 +1,37 @@
+import { motion } from 'framer-motion';
+
 function ToggleButton({ setIsOpen }) {
   return (
     <button onClick={() => setIsOpen((prev) => !prev)}>
-      <svg width="23" height="23" viewBox="0 0 23 23">
-        <path strokeWidth="3" stroke="black" strokeLinecap="round" />
-        <path strokeWidth="3" stroke="black" strokeLinecap="round" />
-        <path strokeWidth="3" stroke="black" strokeLinecap="round" />
+      <svg width="23" height="23" viewBox="0 0 23 15">
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: 'M 2 2.5 L 20 2.5' },
+            open: { d: 'M 3 16.5 L 17 2.5' },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          d="M 2 9.423 L 20 9.423"
+          variants={{
+            closed: { opacity: 1 },
+            open: { opacity: 0 },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: 'M 2 16.346 L 20 16.346' },
+            open: { d: 'M 3 2.5 L 17 16.346' },
+          }}
+        />
       </svg>
     </button>
   );
